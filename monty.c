1c5a96362408bd5c9ea80854842e5d85f76ec966
jstjorje <jstjorje@gmail.com>
2023-03-23 14:56:29 -0700

    push, pull

--- monty.c
+++ monty.c
@@ -1,24 +1,32 @@
 #include "monty.h"
-global_var var_global;
-/**
- * main - driver function for monty program
- * @ac: int num of arguments
- * @av: opcode file
- * Return: 0
- */
-int main(int ac, char **av)
-{
-	stack_t *stack;
 
-	stack = NULL;
-	if (ac != 2)
+list_t *list_opcode = NULL;
+
+/**
+ * main - Main entry point for Stacks and Queues
+ * @argc: Argument count for the matrix
+ * @argv: Argument vector for the matrix
+ * Return: 0 on success or other number if it fails
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+	list_t *temp;
+
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	read_file(av[1], &stack);
-    /* recordar liberar memorias */
-	free_dlistint(stack);
+	check_file(argv[1]);
+	get_file(argv[1]);
+
+	temp = list_opcode;
+
+	for (; temp; temp = temp->next)
+		(*ptr_opcode(temp))(&stack, temp->n);
+
+	free_all(list_opcode, stack);
 	return (0);
 }
